5f527fd053132ef6cdd4ac0840e219172b724295
jeffrepo <jeffer.leo93@gmail.com>
2025-08-19 17:22:00 -0600

    Campos nuevos

--- models/res_partner.py
+++ models/res_partner.py
@@ -9,3 +9,5 @@ class ResPartner(models.Model):
 
     total_due = fields.Monetary(groups='account.group_account_readonly,account.group_account_invoice,sales_team.group_sale_salesman')
     followup_status = fields.Selection(groups='account.group_account_readonly,account.group_account_invoice,sales_team.group_sale_salesman')
+    visitas = fields.Char('Visitas')
+    observaciones = fields.Char('Observaciones')
